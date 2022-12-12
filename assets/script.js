7b599b5a8d895c08c8fdedaa45f835c8df3f4fd8
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-12-12 13:43:44 +0100

    Allow for Fruit creation, randomized positionning.

--- assets/script.js
+++ assets/script.js
@@ -4,23 +4,36 @@ let snakeGame = document.getElementById("snakeGrid");
 console.log(snakeGame.offsetHeight);
 
 let head = document.createElement("div");
+let snakeFruit = document.createElement("div");
+
 
 let headX = (snakeGame.offsetWidth / 2) - (head.offsetWidth / 2);
 let headY = (snakeGame.offsetWidth / 2) - (head.offsetWidth / 2);
 let moveOffset;
+let headWidth;
+let headHeight;
+let fruitX;
+let fruitY;
+let fruitWidth;
+let fruitHeight;
 let timer = 500; 
-console.log(moveOffset);
 
 function createSnake(){
-   
+
     head.setAttribute("id","snakeHead");
     snakeGame.appendChild(head);
     head.style.left = `${headX}px`;
     head.style.top = `${headY}px`;
+    head.style.width = `${10}px`;
+    head.style.height = `${10}px`;
     moveOffset = head.offsetWidth;
+
 }
 
-startGame.addEventListener('click', createSnake);
+startGame.addEventListener('click', () => {
+    createSnake();
+    createFruit();}
+    );
 
 let direction = ""; 
 
@@ -52,11 +65,15 @@ function movingSnake (direction){
      headX < 0 || 
      headY < 0 ||
      (headY + moveOffset) > snakeGame.offsetHeight){
-        console.log('Offboundaries');
         removeSnake();
        
     } else {
-        console.log("In boundaries");
+    }
+
+    if ( headX > fruitX && headX < (fruitX + fruitWidth) && headY > fruitY && headY < (fruitY + fruitHeight)){
+        console.log("Touched!");
+    } else {
+        console.log("Missed");
     }
 }
 
@@ -64,9 +81,28 @@ let timing = setInterval(() => {movingSnake(direction)}, timer);
 
 function removeSnake(){
     head.remove();
+    headWidth = snakeGame.offsetWidth/10;
+    headHeight = snakeGame.offsetHeight/10;
+    head.style.width = `${headWidth}px`;
+    head.style.height = `${headHeight}px`;
     headX = (snakeGame.offsetWidth / 2) - (head.offsetWidth / 2);
     headY = (snakeGame.offsetWidth / 2) - (head.offsetWidth / 2);
     clearInterval(timing);
+}
+
+function createFruit(){
+    snakeFruit.setAttribute("id","snakeFruit");
+    snakeFruit.style.width = `${10}px`;
+    snakeFruit.style.height = `${10}px`;
+    fruitX = (Math.random() * (( snakeGame.offsetWidth) - ( 0 + moveOffset))) + moveOffset;
+    fruitY = (Math.random() * (( snakeGame.offsetHeight) - ( 0 + moveOffset))) + moveOffset;
+    snakeFruit.style.left = `${fruitX}px`;
+    snakeFruit.style.top = `${fruitY}px`;
+    console.log(snakeFruit.style.top);
+    console.log(snakeFruit.style.left);
+    snakeGame.appendChild(snakeFruit);
+    fruitWidth = 10;
+    fruitHeight = 10;
 }
 
 window.addEventListener('keydown', (e) =>{
