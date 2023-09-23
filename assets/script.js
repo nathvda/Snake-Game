90089c16857736e674e16f7992771b420bf52247
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-09-23 15:02:14 +0200

    refactor: saving the game

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,19 @@
 let startGame = document.getElementById("startGame");
 let snakeGame = document.getElementById("snakeGrid");
 
-let gameWidth = snakeGame.style.width = Math.floor((snakeGame.offsetHeight/100))*100;
-let gameHeight = snakeGame.style.height = Math.floor((snakeGame.offsetWidth/100))*100;
+let gameWidth = (snakeGame.style.width =
+  Math.floor(snakeGame.offsetHeight / 100) * 100);
+let gameHeight = (snakeGame.style.height =
+  Math.floor(snakeGame.offsetWidth / 100) * 100);
 
 let head = document.createElement("div");
 let snakeFruit = document.createElement("div");
 let tailBox = document.createElement("div");
-tailBox.setAttribute("id","tail");
-snakeGame.append(tailBox);    
+tailBox.setAttribute("id", "tail");
+snakeGame.append(tailBox);
 
-let headX = (gameWidth / 2) - (head.offsetWidth / 2);
-let headY = (gameHeight / 2) - (head.offsetHeight / 2);
+let headX = gameWidth / 2 - head.offsetWidth / 2;
+let headY = gameHeight / 2 - head.offsetHeight / 2;
 let headWidth;
 
 let headHeight;
@@ -31,182 +33,171 @@ let previousY;
 const tail = [];
 let timing;
 
-let timer = 150; 
-let direction; 
+let timer = 150;
+let direction;
 let score = 0;
 
 let scoreBox = document.getElementById("score");
 
-function newGame(){
-
-    clearInterval(timing);
-    direction = "";  
-    head.setAttribute("id","snakeHead");
-    snakeGame.appendChild(head);
-    head.style.left = `${headX}px`;
-    head.style.top = `${headY}px`;
-    head.style.width = `${10}px`;
-    head.style.height = `${10}px`;
-    moveOffset = head.offsetWidth;
-    timing = setInterval(() => {movingSnake(direction)}, timer);
-    score = 0;
-    scoreBox.innerHTML = `Score : ${score}`;
-    createFruit();
-
+function newGame() {
+  clearInterval(timing);
+  direction = "";
+  head.setAttribute("id", "snakeHead");
+  snakeGame.appendChild(head);
+  head.style.left = `${headX}px`;
+  head.style.top = `${headY}px`;
+  head.style.width = `${10}px`;
+  head.style.height = `${10}px`;
+  moveOffset = head.offsetWidth;
+  timing = setInterval(() => {
+    movingSnake(direction);
+  }, timer);
+  score = 0;
+  scoreBox.innerHTML = `Score : ${score}`;
+  createFruit();
 }
 
-startGame.addEventListener('click', () => {
-    newGame();
-}
-    );
-
-
-function movingSnake (direction){
-
-
-
-    let tailElements = document.getElementById("tail").querySelectorAll("div");
-
-for (let i = 0 ; i < tailElements.length ; i++){
-
-    
-
-previousX = headX;
-previousY = headY;
-
-if (i == 0){
-tailElements[0].style.left = `${previousX}px`;
-tailElements[0].style.top = `${previousY}px`;
-tailElements[i].style.display = "block";
-}
-
-if (i > 0){
-    
-tailElements[i-1].style.left = tailElements[i].style.left;
-tailElements[i-1].style.top = tailElements[i].style.top;
-tailElements[i].style.left = valueX;
-tailElements[i].style.top = valueY;
-tailElements[i].style.display = "block";
-
-}
-
-valueX = tailElements[i].style.left;
-valueY = tailElements[i].style.top;
-}
-
-
-    switch (direction){
-
-        case "RIGHT" : 
-        headX += moveOffset;
-        head.style.left = `${headX}px`;
-        break;
-
-        case "UP" : 
-        headY -= moveOffset;
-        head.style.top = `${headY}px`;
-        break;
-
-        case "LEFT": 
-        headX -= moveOffset;
-        head.style.left = `${headX}px`;
-        break;
-
-        case "DOWN": 
-        headY += moveOffset;
-        head.style.top = `${headY}px`;
-        break;
-    }
-
-    for(let i = 0 ; i < tailElements.length ; i++){
-        console.log(tailElements[i].style.left);
-        console.log(tailElements[i].style.top);
-
-        if ((head.style.left == tailElements[i].style.left) && (head.style.top == tailElements[i].style.top)){
-           removeSnake();
-        }
-    }
-
-    if ((headX + moveOffset) > gameWidth ||
-     headX < 0 || 
-     headY < 0 ||
-     (headY + moveOffset) > gameHeight){
-        removeSnake();
-        alert(`You lost ! Your score is : ${score}`);
-        alert(`Snake length : ${tailElements.length+1}`);
-       
-    } else {
-    }
-
-    if (fruitX == headX && fruitY == headY){
-        score += 20;
-        scoreBox.innerHTML = `Score : ${score}`;
-        createFruit();
-        addSnakeTail();
-    } else {
-
-    }
-
-}
-
-function addSnakeTail(){
-        let tailItem = document.createElement("div");
-        tailItem.style.width = `${10}px`;
-        tailItem.style.height = `${10}px`;
-        tailBox.appendChild(tailItem);
-        tailItem.style.display = "none";
-
-}
-
-function removeSnake(){
-    head.remove();
-    tailBox.innerHTML = "";
-    snakeFruit.remove();
-    headWidth = gameWidth/10;
-    headHeight = gameHeight/10;
-    head.style.width = `${headWidth}px`;
-    head.style.height = `${headHeight}px`;
-    headX = Math.floor((gameWidth / 2) - (head.offsetWidth / 2));
-    headY = Math.floor((gameHeight/ 2) - (head.offsetWidth / 2));
-    clearInterval(timing);
-}
-
-function createFruit(){
-    snakeFruit.setAttribute("id","snakeFruit");
-    snakeFruit.style.width = `${10}px`;
-    snakeFruit.style.height = `${10}px`;
-    fruitX = Math.floor((Math.random() * gameWidth)/10)*10;
-    fruitY = Math.floor((Math.random() * gameHeight)/10)*10;
-    snakeFruit.style.left = `${fruitX}px`;
-    snakeFruit.style.top = `${fruitY}px`;
-
-    snakeGame.appendChild(snakeFruit);
-    fruitWidth = 10;
-    fruitHeight = 10;
-}
-
-window.addEventListener('keydown', (e) =>{
-
-    switch (e.code){
-        case "KeyL" : 
-        console.log("going right");
-        direction = "RIGHT";
-        break;
-
-        case "KeyI" : 
-        console.log("going up");
-        direction = "UP";
-        break;
-
-        case "KeyJ" : 
-        console.log("going left");
-        direction = "LEFT";
-        break;
-
-        case "Comma":
-        console.log("going down");
-        direction = "DOWN";
-        break;
-    }
+startGame.addEventListener("click", () => {
+  newGame();
 });
 
+function movingSnake(direction) {
+  let tailElements = document.getElementById("tail").querySelectorAll("div");
+
+  for (let i = 0; i < tailElements.length; i++) {
+    previousX = headX;
+    previousY = headY;
+
+    if (i == 0) {
+      tailElements[0].style.left = `${previousX}px`;
+      tailElements[0].style.top = `${previousY}px`;
+      tailElements[i].style.display = "block";
+    }
+
+    if (i > 0) {
+      tailElements[i - 1].style.left = tailElements[i].style.left;
+      tailElements[i - 1].style.top = tailElements[i].style.top;
+      tailElements[i].style.left = valueX;
+      tailElements[i].style.top = valueY;
+      tailElements[i].style.display = "block";
+    }
+
+    valueX = tailElements[i].style.left;
+    valueY = tailElements[i].style.top;
+  }
+
+  switch (direction) {
+    case "RIGHT":
+      headX += moveOffset;
+      head.style.left = `${headX}px`;
+      break;
+
+    case "UP":
+      headY -= moveOffset;
+      head.style.top = `${headY}px`;
+      break;
+
+    case "LEFT":
+      headX -= moveOffset;
+      head.style.left = `${headX}px`;
+      break;
+
+    case "DOWN":
+      headY += moveOffset;
+      head.style.top = `${headY}px`;
+      break;
+  }
+
+  for (let i = 0; i < tailElements.length; i++) {
+    console.log(tailElements[i].style.left);
+    console.log(tailElements[i].style.top);
+
+    if (
+      head.style.left == tailElements[i].style.left &&
+      head.style.top == tailElements[i].style.top
+    ) {
+      removeSnake();
+    }
+  }
+
+  if (
+    headX + moveOffset > gameWidth ||
+    headX < 0 ||
+    headY < 0 ||
+    headY + moveOffset > gameHeight
+  ) {
+    removeSnake();
+    alert(`You lost ! Your score is : ${score}`);
+    alert(`Snake length : ${tailElements.length + 1}`);
+  } else {
+  }
+
+  if (fruitX == headX && fruitY == headY) {
+    score += 20;
+    scoreBox.innerHTML = `Score : ${score}`;
+    createFruit();
+    addSnakeTail();
+  } else {
+  }
+}
+
+function addSnakeTail() {
+  let tailItem = document.createElement("div");
+  tailItem.style.width = `${10}px`;
+  tailItem.style.height = `${10}px`;
+  tailBox.appendChild(tailItem);
+  tailItem.style.display = "none";
+  tailItem.style.left = `${headX}px`;
+  tailItem.style.top = `${headY}px`;
+}
+
+function removeSnake() {
+  head.remove();
+  tailBox.innerHTML = "";
+  snakeFruit.remove();
+  headWidth = gameWidth / 10;
+  headHeight = gameHeight / 10;
+  head.style.width = `${headWidth}px`;
+  head.style.height = `${headHeight}px`;
+  headX = Math.floor(gameWidth / 2 - head.offsetWidth / 2);
+  headY = Math.floor(gameHeight / 2 - head.offsetWidth / 2);
+  clearInterval(timing);
+}
+
+function createFruit() {
+  snakeFruit.setAttribute("id", "snakeFruit");
+  snakeFruit.style.width = `${10}px`;
+  snakeFruit.style.height = `${10}px`;
+  fruitX = Math.floor((Math.random() * gameWidth) / 10) * 10;
+  fruitY = Math.floor((Math.random() * gameHeight) / 10) * 10;
+  snakeFruit.style.left = `${fruitX}px`;
+  snakeFruit.style.top = `${fruitY}px`;
+
+  snakeGame.appendChild(snakeFruit);
+  fruitWidth = 10;
+  fruitHeight = 10;
+}
+
+window.addEventListener("keydown", (e) => {
+  switch (e.code) {
+    case "KeyL":
+      console.log("going right");
+      direction = "RIGHT";
+      break;
+
+    case "KeyI":
+      console.log("going up");
+      direction = "UP";
+      break;
+
+    case "KeyJ":
+      console.log("going left");
+      direction = "LEFT";
+      break;
+
+    case "Comma":
+      console.log("going down");
+      direction = "DOWN";
+      break;
+  }
+});
